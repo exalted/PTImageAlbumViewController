1b9dc22040e094538a2b4c715a9b1678ac72f6c4
Ali Servet Donmez <asd@pittle.org>
2012-02-16 18:39:52 +0100

    Add empty view controller implementation

---
+++ PTImageAlbumViewController/PTImageAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTImageAlbumViewController.h
+//  AlbumDemo
+//
+//  Created by Ali Servet Donmez on 16.2.12.
+//  Copyright (c) 2012 Apex-net srl. All rights reserved.
+//
+
+#import "NIToolbarPhotoViewController.h"
+
+@interface PTImageAlbumViewController : NIToolbarPhotoViewController
+
+@end
