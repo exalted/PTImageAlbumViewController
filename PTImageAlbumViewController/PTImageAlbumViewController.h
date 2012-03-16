6f4a8f273ac1c4db3b588d04c3a9e177be64b346
Ali Servet Donmez <asd@pittle.org>
2012-03-16 18:15:42 +0100

    Add copyright notice and statement of copying permission to each source file

--- PTImageAlbumViewController/PTImageAlbumViewController.h
+++ PTImageAlbumViewController/PTImageAlbumViewController.h
@@ -1,9 +1,19 @@
 //
-//  PTImageAlbumViewController.h
-//  AlbumDemo
+// Copyright (C) 2012 Ali Servet Donmez. All rights reserved.
 //
-//  Created by Ali Servet Donmez on 16.2.12.
-//  Copyright (c) 2012 Apex-net srl. All rights reserved.
+// This file is part of PTImageAlbumViewController.
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PTImageAlbumViewController is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PTImageAlbumViewController. If not, see <http://www.gnu.org/licenses/>.
+// PTImageAlbumViewController is free software: you can redistribute it and/or
 //
 
 #import "NetworkPhotoAlbumViewController.h"
