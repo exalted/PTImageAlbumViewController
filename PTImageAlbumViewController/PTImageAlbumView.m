ea13c512bedba986460780c4f7aa941ab0126caf
Ali Servet Donmez <asd@pittle.org>
2012-06-19 18:37:27 +0200

    Cache values returned from data source for better performance

--- PTImageAlbumViewController/PTImageAlbumView.m
+++ PTImageAlbumViewController/PTImageAlbumView.m
@@ -21,7 +21,7 @@
 #import "PTImageAlbumViewDataSource.h"
 
 @interface PTImageAlbumView () {
-    NSInteger _numberOfImages;
+    NSMutableArray *_cachedData;
 }
 
 @end
@@ -34,65 +34,88 @@
 
 - (NSInteger)numberOfImages
 {
-    if (!_numberOfImages) {
-        _numberOfImages = [self.imageAlbumDataSource numberOfImagesInAlbumView:self];
+    if (_cachedData == nil) {
+        NSInteger numberOfImages = [self.imageAlbumDataSource numberOfImagesInAlbumView:self];
+        _cachedData = [[NSMutableArray alloc] initWithCapacity:numberOfImages];
+        for (NSInteger i = 0; i < numberOfImages; i++) {
+            [_cachedData addObject:[NSMutableDictionary dictionary]];
+        }
     }
-    return _numberOfImages;
+    return [_cachedData count];
 }
 
 - (CGSize)sizeForImageAtIndex:(NSInteger)index
 {
-    if ([self.imageAlbumDataSource respondsToSelector:@selector(imageAlbumView:sizeForImageAtIndex:)]) {
-        return [self.imageAlbumDataSource imageAlbumView:self sizeForImageAtIndex:index];
-    }
-    else {
-        NSString *source = [self.imageAlbumDataSource imageAlbumView:self sourceForImageAtIndex:index];
-        
-        // TODO remove duplicate
-        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        NSURL *url = nil;
-        
-        // Check for file URLs.
-        if ([source hasPrefix:@"/"]) {
-            // If the url starts with / then it's likely a file URL, so treat it accordingly.
-            url = [NSURL fileURLWithPath:source];
+    NSValue *size = [[_cachedData objectAtIndex:index] objectForKey:@"size"];
+    if (size == nil) {
+        if ([self.imageAlbumDataSource respondsToSelector:@selector(imageAlbumView:sizeForImageAtIndex:)]) {
+            size = [NSValue valueWithCGSize:[self.imageAlbumDataSource imageAlbumView:self sizeForImageAtIndex:index]];
         }
         else {
-            // Otherwise we assume it's a regular URL.
-            url = [NSURL URLWithString:source];
-        }
-        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-        
-        CGImageSourceRef imageSource = CGImageSourceCreateWithURL((__bridge CFURLRef)url, NULL);
-        if (imageSource) {
-            NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
-                                     [NSNumber numberWithBool:NO], (NSString *)kCGImageSourceShouldCache, 
-                                     nil];
+            NSString *source = [self sourceForImageAtIndex:index];
             
-            CFDictionaryRef imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, (__bridge CFDictionaryRef)options);
-            CFRelease(imageSource);
+            // TODO remove duplicate
+            // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            NSURL *url = nil;
+            
+            // Check for file URLs.
+            if ([source hasPrefix:@"/"]) {
+                // If the url starts with / then it's likely a file URL, so treat it accordingly.
+                url = [NSURL fileURLWithPath:source];
+            }
+            else {
+                // Otherwise we assume it's a regular URL.
+                url = [NSURL URLWithString:source];
+            }
+            // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-            if (imageProperties) {
-                NSNumber *width = (__bridge NSNumber *)CFDictionaryGetValue(imageProperties, kCGImagePropertyPixelWidth);
-                NSNumber *height = (__bridge NSNumber *)CFDictionaryGetValue(imageProperties, kCGImagePropertyPixelHeight);
-                CFRelease(imageProperties);
-                
-                return CGSizeMake([width floatValue], [height floatValue]);
+            // we will try to get image size from image source only if this is
+            // an image file available on the file system
+            if (url.isFileURL) {
+                CGImageSourceRef imageSource = CGImageSourceCreateWithURL((__bridge CFURLRef)url, NULL);
+                if (imageSource) {
+                    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                                             [NSNumber numberWithBool:NO], (NSString *)kCGImageSourceShouldCache, 
+                                             nil];
+                    
+                    CFDictionaryRef imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, (__bridge CFDictionaryRef)options);
+                    CFRelease(imageSource);
+                    
+                    if (imageProperties) {
+                        NSNumber *width = (__bridge NSNumber *)CFDictionaryGetValue(imageProperties, kCGImagePropertyPixelWidth);
+                        NSNumber *height = (__bridge NSNumber *)CFDictionaryGetValue(imageProperties, kCGImagePropertyPixelHeight);
+                        CFRelease(imageProperties);
+                        
+                        size = [NSValue valueWithCGSize:CGSizeMake([width floatValue], [height floatValue])];
+                    }
+                }
             }
         }
+
+        [[_cachedData objectAtIndex:index] setObject:(size ? size : [NSValue valueWithCGSize:CGSizeZero]) forKey:@"size"];
     }
-    
-    return CGSizeZero;
+
+    return [size CGSizeValue];
 }
 
 - (NSString *)sourceForImageAtIndex:(NSInteger)index
 {
-    return [self.imageAlbumDataSource imageAlbumView:self sourceForImageAtIndex:index];
+    NSString *source = [[_cachedData objectAtIndex:index] objectForKey:@"source"];
+    if (source == nil) {
+        source = [self.imageAlbumDataSource imageAlbumView:self sourceForImageAtIndex:index];
+        [[_cachedData objectAtIndex:index] setObject:source forKey:@"source"];
+    }
+    return source;
 }
 
 - (NSString *)sourceForThumbnailImageAtIndex:(NSInteger)index
 {
-    return [self.imageAlbumDataSource imageAlbumView:self sourceForThumbnailImageAtIndex:index];
+    NSString *source = [[_cachedData objectAtIndex:index] objectForKey:@"thumbnailSource"];
+    if (source == nil) {
+        source = [self.imageAlbumDataSource imageAlbumView:self sourceForThumbnailImageAtIndex:index];
+        [[_cachedData objectAtIndex:index] setObject:source forKey:@"thumbnailSource"];
+    }
+    return source;
 }
 
 @end
